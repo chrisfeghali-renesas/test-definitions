e1c8d24cf2060a1d08cfa2a5e171d94d586aba63
chrisfeghali-renesas <52493189+chrisfeghali-renesas@users.noreply.github.com>
2019-07-24 10:21:23 +0100

    Change bw_mem to variable memory
    Memory now depends on total available memory

--- automated/linux/lmbench/lmbench-memory.sh
+++ automated/linux/lmbench/lmbench-memory.sh
@@ -5,12 +5,18 @@ OUTPUT="$(pwd)/output"
 RESULT_FILE="${OUTPUT}/result.txt"
 export RESULT_FILE
 
+TOTAL_MEMORY=`dmesg | grep "Memory: " | awk '{print $2}' | awk -F "/" '{print $2}' | awk '{print substr($0, 1, length($0)-1)}'`
+echo "Total memory available is $TOTAL_MEMORY K"                                 
+MEMORY_MB=$(($TOTAL_MEMORY / 1000))                                              
+BANDWIDTH_MEMORY=$(($MEMORY_MB * 4 / 10))                                        
+echo "Bandwidth memory is "$BANDWIDTH_MEMORY"MB"               
+
 bandwidth_test() {
     test_list="rd wr rdwr cp frd fwr fcp bzero bcopy"
     for test in ${test_list}; do
         # bw_mem use MB/s as units.
         # shellcheck disable=SC2154
-        ./bin/"${abi}"/bw_mem 128m "$test" 2>&1 \
+        ./bin/"${abi}"/bw_mem "$BANDWIDTH_MEMORY"m "$test" 2>&1 \
           | awk -v test_case="memory-${test}-bandwidth" \
             '{printf("%s pass %s MB/s\n", test_case, $2)}' \
           | tee -a "${RESULT_FILE}"
